7a2538976a5800d7798c914c8a8a3330d7bb9b83
Paulo Hainosz <phshainosz@hotmail.com>
2019-09-24 18:33:45 -0300

    Formulários e eventos do clique iniciados o desenvolvimento.

---
+++ ControleDeEstoqueUP/frmCliente.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControleDeEstoqueUP
+{
+    /// <summary>
+    /// Interaction logic for frmCliente.xaml
+    /// </summary>
+    public partial class frmCliente : Window
+    {
+        public frmCliente()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnAdicionar_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnLocalizar_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnExcluir_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
---
+++ ControleDeEstoqueUP/frmFuncionario.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControleDeEstoqueUP
+{
+    /// <summary>
+    /// Interaction logic for frmFuncionario.xaml
+    /// </summary>
+    public partial class frmFuncionario : Window
+    {
+        public frmFuncionario()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnLocalizar_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnExcluir_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
