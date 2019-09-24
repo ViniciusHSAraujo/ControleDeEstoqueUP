9ec4cb13772475123d647870ecfb74cb8d81f4ba
Paulo Hainosz <phshainosz@hotmail.com>
2019-09-24 19:59:22 -0300

    falta arrumar os botões.

--- ControleDeEstoqueUP/frmFuncionario.xaml.cs
+++ ControleDeEstoqueUP/frmFuncionario.xaml.cs
@@ -13,13 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ControleDeEstoqueUP
-{
+namespace ControleDeEstoqueUP {
     /// <summary>
     /// Interaction logic for frmFuncionario.xaml
     /// </summary>
-    public partial class frmFuncionario : Window
-    {
+    public partial class frmFuncionario : Window {
         /**
          * Operações:
          *  0: Nenhuma operação.
@@ -29,29 +27,66 @@ namespace ControleDeEstoqueUP
          */
         int operacao;
         FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-        public frmFuncionario()
-        {
+        public frmFuncionario() {
             InitializeComponent();
+            ModificarBotoesFormulario(0);
         }
 
         private void btnAdicionar_Click(object sender, RoutedEventArgs e) {
-
+            ModificarBotoesFormulario(1);
         }
 
         private void btnLocalizar_Click(object sender, RoutedEventArgs e) {
-
+            ModificarBotoesFormulario(1);
         }
 
         private void btnExcluir_Click(object sender, RoutedEventArgs e) {
-
+            ModificarBotoesFormulario(1);
         }
 
         private void btnSalvar_Click(object sender, RoutedEventArgs e) {
-
+            ModificarBotoesFormulario(1);
         }
 
         private void btnCancelar_Click(object sender, RoutedEventArgs e) {
+            ModificarBotoesFormulario(1);
+        }
 
+        private void LimparCampos() {
+            txtCodigo.Clear();
+            txtNome.Clear();
+        }
+
+        private void ModificarBotoesFormulario(int operacao) {
+            switch (operacao) {
+                case 0: //Inicial, somente o botão de adicionar e localizar ativos..
+                    btnAdicionar.IsEnabled = true;
+                    btnLocalizar.IsEnabled = true;
+                    btnEditar.IsEnabled = false;
+                    btnExcluir.IsEnabled = false;
+                    btnSalvar.IsEnabled = false;
+                    btnCancelar.IsEnabled = false;
+                    panelContent.IsEnabled = false;
+                    break;
+                case 1: //Botão ADICIONAR clicado, somente o botão de adicionar e localizar ativos..
+                    btnAdicionar.IsEnabled = false;
+                    btnLocalizar.IsEnabled = false;
+                    btnEditar.IsEnabled = false;
+                    btnExcluir.IsEnabled = false;
+                    btnSalvar.IsEnabled = true;
+                    btnCancelar.IsEnabled = true;
+                    panelContent.IsEnabled = true;
+                    break;
+                case 2: //Botão LOCALIZAR clicado, somente o botão de excluir, editar e cancelar ativos..
+                    btnAdicionar.IsEnabled = false;
+                    btnLocalizar.IsEnabled = false;
+                    btnEditar.IsEnabled = true;
+                    btnExcluir.IsEnabled = true;
+                    btnSalvar.IsEnabled = false;
+                    btnCancelar.IsEnabled = true;
+                    panelContent.IsEnabled = true;
+                    break;
+            }
         }
     }
 }
